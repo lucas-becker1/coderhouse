8770fadeda9369b0029f031da792e8d32e6e1671
lucas-becker1 <lucas.becker.14.lb@gmail.com>
2022-06-17 14:52:13 -0300

    desafio

---
+++ ciclos.js
@@ -0,0 +1,11 @@
+debugger
+
+let infoUser =(prompt("Ingresar nombre y apellido"))
+
+for (let i=1; i<=50; i++) {
+    document.write("Hola "+infoUser+" su turno es el "+ i + "<br>" );
+    break;
+}
+
+
+
